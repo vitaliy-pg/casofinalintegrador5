09162227d6f4751fb62150b93357d89770547fee
vitaliy-pg <aljndrvt@myuax.com>
2024-04-14 12:58:22 +0200

    añado la segunda parte el main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,40 @@
 
 import java.util.ArrayList;
 import Análisis.AnalisisGenomico;
+import bAnalisisnumerico.FuncionesNumericas;
 
 import static Análisis.AnalisisGenomico.contarGenes;
 
 public class Main {
     public static void main(String[] args) {
-        String cadenaADN = "ATGATGATGTAG        "   ;
-        int cantidadGenes = contarGenes(cadenaADN);
-        System.out.println("Cantidad de genes en la cadena de ADN: " + cantidadGenes);
-        String[] alelos = { "A", "T", "C", "G" };
+        String cadenaADN = "ATGATGATGTAG        ";
+        int cantidadGenes = contarGenes ( cadenaADN );
+        System.out.println ( "Cantidad de genes en la cadena de ADN: " + cantidadGenes );
+        String[] alelos = {"A", "T", "C", "G"};
         int longitud = 3;
-        ArrayList<String> combinaciones = AnalisisGenomico.calcularCombinacionesGeneticas(alelos, longitud);
-        System.out.println("Combinaciones genéticas de longitud " + longitud + ":");
+        ArrayList<String> combinaciones = AnalisisGenomico.calcularCombinacionesGeneticas ( alelos, longitud );
+        System.out.println ( "Combinaciones genéticas de longitud " + longitud + ":" );
         for (String combinacion : combinaciones) {
-            System.out.println(combinacion);
+            System.out.println ( combinacion );
         }
+    }
+
+    public static int contarGenes ( String cadenaADN ) {
+        int n = 5;
+        System.out.println("Suma de los primeros " + n + " números naturales: " + FuncionesNumericas.sumaNaturales(n));
+
+        int inicio = 3;
+        int fin = 7;
+        ArrayList<Integer> numerosEnRango = FuncionesNumericas.listarNumerosEnRango(inicio, fin);
+        System.out.println("Números en el rango [" + inicio + ", " + fin + "]: " + numerosEnRango);
+
+        int base = 2;
+        int exponente = 5;
+        System.out.println("Potencia de " + base + " elevado a " + exponente + ": " + FuncionesNumericas.calcularPotencia(base, exponente));
+
+        int[] datos = {3, 7, 2, 9, 5};
+        System.out.println("Máximo valor en el conjunto de datos: " + FuncionesNumericas.encontrarMaximo(datos));
+        return n;
     }
 }
 
