8c606060fb35b4bd05e27ae50472b7c0c49c27ab
vitaliy-pg <aljndrvt@myuax.com>
2024-04-14 13:29:13 +0200

    parte de gestion de fechas

--- src/main/java/Informaciongenetica/busquedadetexto.java
+++ src/main/java/Informaciongenetica/busquedadetexto.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class BusquedaEnTexto {
+public class busquedadetexto {
     public static boolean buscarLineal(String nombreArchivo, String palabra) {
         try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
---
+++ src/main/java/Informaciongenetica/fecha.java
@@ -0,0 +1,10 @@
+package Informaciongenetica;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class GestionFechas {
+    public static void ordenarFechas(List<LocalDate> fechas) {
+        Collections.sort(fechas);
+    }
+}
